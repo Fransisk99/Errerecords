2179e9d56650b52a821f0189b16685cb842d7eac
Piantagrassa <pianta_grassa@email.com>
2024-09-07 12:24:23 +0200

    Fatto altre modifiche alle foto e ai video

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Seleziona tutte le immagini
     let images = document.querySelectorAll('.immaginiRandomContainer img');
-
+    let messaggio = document.getElementById('messaggio');
+    
     images.forEach(img => {
-        // Aggiungi un gestore di eventi click per ciascuna immagine
         img.addEventListener('click', () => {
-            
-            // Rimuovi la classe 'ingrandita' da tutte le immagini
-            images.forEach(i => i.classList.remove('ingrandita'));
-            
-            // Aggiungi la classe 'ingrandita' solo all'immagine cliccata
-            img.classList.add('ingrandita');
+            if (img.classList.contains('ingrandita')) {
+                img.classList.remove('ingrandita');
+                messaggio.style.display = 'none';
+            } else {
+                images.forEach(i => {
+                    i.classList.remove('ingrandita');
+                });
+                img.classList.add('ingrandita');
+                messaggio.style.display = 'block';
+            }
         });
     });
 });
